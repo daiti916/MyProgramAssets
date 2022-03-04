a9120307da6e6104df65ddb7329aa54dbf042b3e
daiti916 <liberty7heaven@gmail.com>
2022-03-04 20:15:53 +0900

    ・時間 / 回数を5パターン設定できるように修正

--- Auto_mrkr/index.py
+++ Auto_mrkr/index.py
@@ -7,7 +7,7 @@ from lib import mouse
 
 # 画面作成
 tk = tkinter.Tk()
-tk.geometry('400x50') # 画面サイズの設定
+tk.geometry("270x300+1300+250") # 画面サイズの設定
 tk.title('自動出品投稿ツール') # 画面タイトルの設定
 
 dict_end_date = {"年": 2022, "月": 4, "日": 5, "時": 9}
@@ -23,37 +23,114 @@ else:
 
     # 自動投稿処理
     def input_kizi_event():
-        time_data = time_data_box.get()
-        cnt_data = cnt_data_box.get()
-        input.input_data(time_data,cnt_data)
+        time1_data = time1_data_box.get()
+        time2_data = time2_data_box.get()
+        time3_data = time3_data_box.get()
+        time4_data = time4_data_box.get()
+        time5_data = time5_data_box.get()
+        cnt1_data = cnt1_data_box.get()
+        cnt2_data = cnt2_data_box.get()
+        cnt3_data = cnt3_data_box.get()
+        cnt4_data = cnt4_data_box.get()
+        cnt5_data = cnt5_data_box.get()
+        input.input_data(time1_data,time2_data,time3_data,time4_data,time5_data,
+                         cnt1_data,cnt2_data,cnt3_data,cnt4_data,cnt5_data)
 
     # マウスポジション取得処理
     def get_mouse_event():
         mouse.mouse_position()
 
 
-    # 出品時間
-    time_label = tkinter.Label(text='時間：')
-    time_label.place(x=30, y=20)
+    # ----- 出品時間 ----- #
+    # 出品1時間
+    time1_label = tkinter.Label(text='時間1：')
+    time1_label.place(x=30, y=20)
 
-    time_data_box = tkinter.Entry(width=5)
-    time_data_box.insert(0, '11')
-    time_data_box.place(x=70, y=20)
+    time1_data_box = tkinter.Entry(width=7)
+    time1_data_box.insert(0, '11:20')
+    time1_data_box.place(x=80, y=20)
 
-    # 下書き投稿回数
-    cnt_num_label = tkinter.Label(text='回数：')
-    cnt_num_label.place(x=120, y=20)
+    # 出品2時間
+    time2_label = tkinter.Label(text='時間2：')
+    time2_label.place(x=30, y=60)
 
-    cnt_data_box = tkinter.Entry(width=5)
-    cnt_data_box.insert(0, '2')
-    cnt_data_box.place(x=160, y=20)
+    time2_data_box = tkinter.Entry(width=7)
+    time2_data_box.insert(0, '12:30')
+    time2_data_box.place(x=80, y=60)
+
+    # 出品3時間
+    time3_label = tkinter.Label(text='時間3：')
+    time3_label.place(x=30, y=100)
+
+    time3_data_box = tkinter.Entry(width=7)
+    time3_data_box.insert(0, '15:30')
+    time3_data_box.place(x=80, y=100)
+
+    # 出品4時間
+    time4_label = tkinter.Label(text='時間4：')
+    time4_label.place(x=30, y=140)
+
+    time4_data_box = tkinter.Entry(width=7)
+    time4_data_box.insert(0, '17:30')
+    time4_data_box.place(x=80, y=140)
+
+    # 出品5時間
+    time5_label = tkinter.Label(text='時間5：')
+    time5_label.place(x=30, y=180)
+
+    time5_data_box = tkinter.Entry(width=7)
+    time5_data_box.insert(0, '21:30')
+    time5_data_box.place(x=80, y=180)
+
+
+    # ----- 投稿回数 ----- #
+    # 回数1
+    cnt1_num_label = tkinter.Label(text='回数1：')
+    cnt1_num_label.place(x=140, y=20)
+
+    cnt1_data_box = tkinter.Entry(width=5)
+    cnt1_data_box.insert(0, '2')
+    cnt1_data_box.place(x=190, y=20)
+
+
+    # 回数2
+    cnt2_num_label = tkinter.Label(text='回数2：')
+    cnt2_num_label.place(x=140, y=60)
+
+    cnt2_data_box = tkinter.Entry(width=5)
+    cnt2_data_box.insert(0, '2')
+    cnt2_data_box.place(x=190, y=60)
+
+    # 回数3
+    cnt3_num_label = tkinter.Label(text='回数3：')
+    cnt3_num_label.place(x=140, y=100)
+
+    cnt3_data_box = tkinter.Entry(width=5)
+    cnt3_data_box.insert(0, '2')
+    cnt3_data_box.place(x=190, y=100)
+
+    # 回数4
+    cnt4_num_label = tkinter.Label(text='回数4：')
+    cnt4_num_label.place(x=140, y=140)
+
+    cnt4_data_box = tkinter.Entry(width=5)
+    cnt4_data_box.insert(0, '2')
+    cnt4_data_box.place(x=190, y=140)
+
+    # 回数5
+    cnt5_num_label = tkinter.Label(text='回数5：')
+    cnt5_num_label.place(x=140, y=180)
+
+    cnt5_data_box = tkinter.Entry(width=5)
+    cnt5_data_box.insert(0, '2')
+    cnt5_data_box.place(x=190, y=180)
 
     # 自動投稿ボタン
     button = tkinter.Button(text="投稿開始",command=input_kizi_event)
-    button.place(x=210, y=15)
+    button.place(x=30, y=230)
 
     # マウスポジション取得ボタン
     mouse_button = tkinter.Button(text="ポジション取得",command=get_mouse_event)
-    mouse_button.place(x=310, y=15)
+    mouse_button.place(x=150, y=230)
 
 tk.mainloop()
--- Auto_mrkr/lib/input.py
+++ Auto_mrkr/lib/input.py
@@ -1,4 +1,4 @@
-def input_data(time_data,cnt_data):
+def input_data(time1_data,time2_data,time3_data,time4_data,time5_data,cnt1_data,cnt2_data,cnt3_data,cnt4_data,cnt5_data):
     import webbrowser
     import time
     import schedule
@@ -11,10 +11,29 @@ def input_data(time_data,cnt_data):
     page_load_time = 3
     action_wait = 1
 
-    job_time = str(time_data) + ":00"
+    job1_time = str(time1_data)
+    job2_time = str(time2_data)
+    job3_time = str(time3_data)
+    job4_time = str(time4_data)
+    job5_time = str(time5_data)
 
-    print("設定時間：　" + str(job_time))
-    print("投稿回数：　" + str(cnt_data) + "回")
+    # ----  設定情報表示 ----- #
+    print("---------------------------------------")
+    print("設定1：時間：　" + str(job1_time))
+    print("設定1：回数：　" + str(cnt1_data) + "回")
+
+    print("設定2：時間：　" + str(job2_time))
+    print("設定2：回数：　" + str(cnt2_data) + "回")
+
+    print("設定3：時間：　" + str(job3_time))
+    print("設定3：回数：　" + str(cnt3_data) + "回")
+
+    print("設定4：時間：　" + str(job4_time))
+    print("設定4：回数：　" + str(cnt4_data) + "回")
+
+    print("設定5：時間：　" + str(job5_time))
+    print("設定5：回数：　" + str(cnt5_data) + "回")
+    print("---------------------------------------")
 
     # --------------------------------------------------
     # configparserの宣言とiniファイルの読み込み
@@ -27,7 +46,7 @@ def input_data(time_data,cnt_data):
 
     config_ini.read(config_ini_path, encoding='sjis')
 
-    def job():
+    def job(cnt_num,set):
         # 下書き存在有無座標
         draft_check_x = int(config_ini['DEFAULT']['draft_check_x'])
         draft_check_y = int(config_ini['DEFAULT']['draft_check_y'])
@@ -36,8 +55,8 @@ def input_data(time_data,cnt_data):
         draft_up_x = int(config_ini['DEFAULT']['draft_up_x'])
         draft_up_y = int(config_ini['DEFAULT']['draft_up_y'])
 
-        for cnt in(range(int(cnt_data))):
-            print(str(int(cnt) + 1) + "回目投稿開始")
+        for cnt in(range(int(cnt_num))):
+            print(str(set) + " : " + str(int(cnt) + 1) + "回目投稿開始")
             # ブラウザを開く。
             driver = webbrowser.open('https://jp.mercari.com/sell/drafts')
 
@@ -102,8 +121,14 @@ def input_data(time_data,cnt_data):
                 break
     
         print("⇒　出品作業終了")
+        print("---------------------------------------")
         
-    schedule.every().day.at(job_time).do(job)
+    schedule.every().day.at(job1_time).do(job,cnt1_data,"設定1")
+    schedule.every().day.at(job2_time).do(job,cnt2_data,"設定2")
+    schedule.every().day.at(job3_time).do(job,cnt3_data,"設定3")
+    schedule.every().day.at(job4_time).do(job,cnt4_data,"設定4")
+    schedule.every().day.at(job5_time).do(job,cnt5_data,"設定5")
+    
 
     # フル稼働
     while True:
